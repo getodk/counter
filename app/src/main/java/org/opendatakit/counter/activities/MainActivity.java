5545a9521ee0db57079e148c30b1eaa79c654c74
grzegorz <gorczykowski@soldevelo.com>
2017-08-03 11:28:24 +0200

    Saving and reading answers

--- app/src/main/java/org/opendatakit/counter/activities/MainActivity.java
+++ app/src/main/java/org/opendatakit/counter/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package org.opendatakit.counter;
+package org.opendatakit.counter.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.opendatakit.counter.R;
 
 public class MainActivity extends AppCompatActivity {
 
