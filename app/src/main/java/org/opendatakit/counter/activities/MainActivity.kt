3213843aadcb79c7728530aadc2905f1797b9873
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-12-20 10:26:06 +0100

    Converted MainActivity to kotlin

---
+++ app/src/main/java/org/opendatakit/counter/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.opendatakit.counter.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.opendatakit.counter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
